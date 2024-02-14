357c66b4164700c1f48d4838ef309a1fba8896b5
Sotatek-MattDang <son.dangngoc@setel.com>
2024-02-14 21:09:07 +0700

    enhance format number

--- helpers/number-helper.ts
+++ helpers/number-helper.ts
@@ -1,4 +1,12 @@
-export function formatNumber(num: number, decimal = 6){
-    const fixedNumber = Number(num).toFixed(decimal);
-    return new Intl.NumberFormat('en-US', {maximumFractionDigits: decimal}).format(Number(fixedNumber));
+export function formatNumber(num: number, defaulDigits = 6){
+  let digits = defaulDigits;
+  if(num < 1 && num > 0){
+    const fixNumberStr =  Number(num).toString();
+    const firstDigit = fixNumberStr.search(/[1-9]/g);
+    if(firstDigit >= defaulDigits + 1){
+      digits = defaulDigits + 2;
+    }
+  }
+  const fixedNumber = Number(num).toFixed(digits);
+  return new Intl.NumberFormat('en-US', {maximumFractionDigits: digits}).format(Number(fixedNumber));
 }
