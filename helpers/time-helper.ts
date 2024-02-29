49b434a553a346e097aff7b3f7f2a8e52b0d75dd
Sotatek-MattDang <son.dangngoc@setel.com>
2024-02-29 15:42:50 +0700

    use dayjs

--- helpers/time-helper.ts
+++ helpers/time-helper.ts
@@ -1,22 +1,21 @@
-import { padStartStr } from './string-helper';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 export const DATE_FORMAT = {
   YYYY_MM_DD_hh_ss: 'YYYY-MM-DD hh:mm',
   YYYY_MM_DD: 'YYYY_MM_DD'
 }
 
-export const formatDate = (date: string | Date, format = DATE_FORMAT.YYYY_MM_DD_hh_ss) => {
-    const d = new Date(date);
-    switch (format) {
-      case DATE_FORMAT.YYYY_MM_DD:
-        return `${d.getFullYear()}-${padStartStr(d.getMonth() + 1)}-${padStartStr(d.getDate())}`;
-      default:
-        return `${d.getFullYear()}-${padStartStr(d.getMonth() + 1)}-${padStartStr(d.getDate())} ${padStartStr(d.getHours())}:${padStartStr(d.getMinutes())}`;
+export const DEFAULT_TIMEZONE = 'Asia/Ho_Chi_Minh';
 
-    }
+export const formatDate = (date: string | Date, format = DATE_FORMAT.YYYY_MM_DD_hh_ss) => {
+  return dayjs(date).tz(DEFAULT_TIMEZONE).format(format);
 }
 
 export const convertDateToDateTimeLocal = (date: string | Date): string => {
-  const d = new Date(date);
-  return `${d.getFullYear()}-${padStartStr(d.getMonth() + 1)}-${padStartStr(d.getDate())}T${padStartStr(d.getHours())}:${padStartStr(d.getMinutes())}`
+  return dayjs(date).format('YYYY-MM-DDThh:mm');
 }
